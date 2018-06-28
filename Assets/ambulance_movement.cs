07bcae1067cf8e800f172d60d1d2dbc94ba255f7
NoahMarkwica <noahmarkwica23@gmail.com>
2018-06-28 17:47:17 -0400

    i wanna commit

---
+++ Assets/ambulance_movement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ambulance_movement : MonoBehaviour {
+
+    public Rigidbody ambulance;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (Input.GetKey("w"))
+        {
+            ambulance.AddForce(-2000 * Time.deltaTime, 0,0);
+        }
+        if(Input.GetKey("a"))
+        {
+          ambulance.AddForce(0,0, -2000 * Time.deltaTime);
+        }
+        if (Input.GetKey("d"))
+        {
+            ambulance.AddForce(0,0, 2000 * Time.deltaTime);
+        }
+        if (Input.GetKey("s"))
+        {
+            ambulance.AddForce(2000 * Time.deltaTime, 0, 0);
+        }
+
+    }
+}
---
+++ Assets/follow.cs
@@ -0,0 +1,13 @@
+﻿
+using UnityEngine;
+
+public class follow : MonoBehaviour {
+
+    public Transform ambulance;
+    public Vector3 offset;
+
+	// Update is called once per frame
+	void Update () {
+        transform.position = ambulance.position + offset;
+	}
+}
