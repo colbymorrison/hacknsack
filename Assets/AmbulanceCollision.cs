1babd5ab307e5ddf03e0c73d601b2bf5e4917a09
Colby Morrison <colby.morrison@ibm.com>
2018-06-28 21:26:23 -0400

    Hit counter

--- Assets/AmbulanceCollision.cs
+++ Assets/AmbulanceCollision.cs
@@ -1,14 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class AmbulanceCollision : MonoBehaviour {
+    private int count;
+    private ambulance_movement ambulance;
+
+    void Start(){
+        GameObject ambulance_object = GameObject.Find("Ambulance");
+        this.ambulance = ambulance_object.GetComponent<ambulance_movement>();
+    }
+
 	private void OnCollisionEnter(Collision coll)
 	{
         GameObject gameObj = coll.gameObject;
-        Debug.Log("Tag: " + gameObj.tag);
 
         if(gameObj.tag == "zombie"){
+            count++;
+            ambulance.setHitCount(count);
             Destroy(gameObj);
         }
 	}
--- Assets/ambulance_movement.cs
+++ Assets/ambulance_movement.cs
@@ -1,14 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class ambulance_movement : MonoBehaviour {
 
     public Rigidbody ambulance;
+    public Text HitCount;
 
 	// Use this for initialization
 	void Start () {
-		
+        setHitCount(0);
 	}
 	
 	// Update is called once per frame
@@ -29,6 +31,9 @@ public class ambulance_movement : MonoBehaviour {
         {
             ambulance.AddForce(2000 * Time.deltaTime, 0, 0);
         }
+    }
 
+    public void setHitCount(int count){
+        this.HitCount.text = "Hits: " + count;
     }
 }
