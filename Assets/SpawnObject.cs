ce8a9e91553f57395e9fe9ef3583026e31278031
Shawn-Veliyathu <shawn.veliyathu@ibm.com>
2018-06-28 22:41:57 -0400

    Created C# scipt to spawn GameObject randomly in a cube.

---
+++ Assets/SpawnObject.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class SpawnObject : MonoBehaviour {
+
+	public GameObject Personprefab; //Object not in the scence
+
+	public Vector3 center;
+	public Vector3 size;
+
+	// Use this for initialization
+	void Start () {
+
+		SpawnFood();
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey(KeyCode.Q)){
+			SpawnFood	();
+		}
+	}
+
+	public void SpawnFood(){
+		Vector3 pos = center + new Vector3(Random.Range(-size.x / 2 , size.x/2), Random.Range(-size.y / 2 , size.y/2), Random.Range(-size.z / 2 , size.z/2));
+		Instantiate(Personprefab, pos, Quaternion.identity);
+	}
+
+	void OnDrawGizmosSelected(){
+
+		Gizmos.color = new Color(1,0,0,0.5f); //Red
+		Gizmos.DrawCube(center, size);
+
+	}
+}
