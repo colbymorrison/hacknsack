f2057a1821be6d3cef34438ffd3f41613043f8ef
Colby Morrison <colby.morrison@ibm.com>
2018-06-29 02:24:32 -0400

    Spawning ethans

---
+++ Assets/SpawnObject.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class SpawnObject : MonoBehaviour
+{
+
+    public GameObject Personprefab; //Object not in the scence
+
+    public Vector3 center;
+    public Vector3 size;
+
+    // Use this for initialization
+    void Start()
+    {
+        InvokeRepeating("SpawnFood", 2.0f, 1.0f);
+    }
+
+    void Test(){
+        Debug.Log("Spawning");
+    }
+
+    public void SpawnFood()
+    {
+        Vector3 pos = center + new Vector3(Random.Range(-size.x / 2, size.x / 2), Random.Range(-size.y / 2, size.y / 2), Random.Range(-size.z / 2, size.z / 2));
+        Instantiate(Personprefab, pos, Quaternion.identity);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = new Color(1, 0, 0, 0.5f); //Red
+        Gizmos.DrawCube(center, size);
+    }
+}
--- Assets/ambulance_movement.cs
+++ Assets/ambulance_movement.cs
@@ -6,11 +6,10 @@ using UnityEngine;
 public class ambulance_movement : MonoBehaviour {
 
     public Rigidbody ambulance;
-    public Text HitCount;
 
 	// Use this for initialization
 	void Start () {
-        setHitCount(0);
+        
 	}
 	
 	// Update is called once per frame
@@ -31,9 +30,5 @@ public class ambulance_movement : MonoBehaviour {
         {
             ambulance.AddForce(2000 * Time.deltaTime, 0, 0);
         }
-    }
-
-    public void setHitCount(int count){
-        this.HitCount.text = "Hits: " + count;
     }
 }
