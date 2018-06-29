288bec9dae0a2ab39321354d9411d037d29b58ec
Colby Morrison <colby.morrison@ibm.com>
2018-06-28 20:07:46 -0400

    ethan dissapearing

---
+++ Assets/AmbulanceCollision.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmbulanceCollision : MonoBehaviour {
+	private void OnCollisionEnter(Collision coll)
+	{
+        GameObject gameObj = coll.gameObject;
+
+        if(gameObj.tag == "zombie"){
+            Destroy(gameObj);
+        }
+	}
+}
